89301c3d1df648bed9cbc624e06756cc615b819e
itc-s22008 <s22008@std.it-college.ac.jp>
2023-01-26 12:31:25 +0000

    189p

--- components/meta.js
+++ components/meta.js
@@ -1,14 +1,10 @@
 import Head from 'next/head'
 
-import { siteMeta } from 'lib/constants'
-const { siteTitle, siteDesc, siteUrl, siteLocate, siteType, siteIcon } = siteMeta
 export default function Meta ({ pageTitle }) {
-  const title = pageTitle ? '${pageTitle} | ${siteTitle}' : siteTitle
-
   return (
     <Head>
-      <title>{pageTitle} | {siteTitle}</title>
-      <meta property='og:title' content={' ${pagetitle} | ${siteTitle}'} />
+      <title>{pageTitle}</title>
+      <meta property='og:title' content={pageTitle} />
     </Head>
   )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Meta from 'components/meta'
+import Head from 'next/head'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
@@ -10,6 +11,9 @@ import eyecatch from 'images/about.jpg'
 export default function About () {
   return (
     <Container>
+      <Head>
+        <title> アバウト </title>
+      </Head>
       <Meta pageTitle='アバウト' />
       <Hero title='About' subtitle='About development activities' />
 

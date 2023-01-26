c81be33ae5577dae0510ff71d5eedd744229c6be
itc-s22008 <s22008@std.it-college.ac.jp>
2023-01-26 08:38:30 +0000

    180p

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Home () {
   return (
     <Container>
+      <Meta pagetitle='ブログ' />
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
     </Container>
   )

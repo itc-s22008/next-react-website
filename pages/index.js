a742476dc41662b59caab4f8c5d5d4e14adce6ed
itc-s22008 <s22008@std.it-college.ac.jp>
2023-02-02 05:40:44 +0000

    p192

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Hero from 'components/hero'
 export default function Home () {
   return (
     <Container>
-      <Meta pagetitle='ブログ' />
+      <Meta />
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
     </Container>
   )

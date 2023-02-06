0ad0e5a8b33441e42be3d4b70bf6ab41a86bd239
itc-s22008 <s22008@std.it-college.ac.jp>
2023-02-06 12:38:54 +0000

    p229

--- components/post-header.js
+++ components/post-header.js
@@ -1,11 +1,18 @@
 import styles from 'styles/post-header.module.css'
+import { FontArticleIcon } frpm '@fortawesome/react-fontawesome'
+import { faClock } from'@fortawesome/free-regular-svg-icons'
 
 export default function PostHeader ({ title, subtitle, publish = '' }) {
   return (
     <div className={styles.stack}>
       <p className={styles.subtitle}>{subtitle}</p>
       <h1 className={styles.publish}>{title}</h1>
-      {publish && <div className={styles.publish}>{publish}</div>}
+      {publish && (
+        <div className={styles.publish}>
+          <FontAsesomeIcon icon{faClock} size='lg' color='var(--gray-25)' />
+          {publish}
+        </div>
+      )} 
     </div>
   )
 }

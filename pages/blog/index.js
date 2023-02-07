5070bc0f4dd5afce33f0cd523ca24c52583b36aa
itc-s22008 <s22008@std.it-college.ac.jp>
2023-02-07 10:43:52 +0000

    p235

---
+++ pages/blog/index.js
@@ -0,0 +1,10 @@
+import Meta from 'components/meta'
+import Container from 'components/meta'
+
+export default function Blog () {
+  return (
+    <Container>
+      <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
+    </Container>
+  )
+}
